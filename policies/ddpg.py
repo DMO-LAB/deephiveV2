bfd27ba125adbf47a7845703680dd1cb32661e61
elotech47 <eloghosaefficiency@gmail.com>
2023-11-29 09:48:12 -0600

    new branch - split-agent

---
+++ policies/ddpg.py
@@ -0,0 +1,108 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import copy
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+        self.layer_1 = nn.Linear(state_dim, 400)
+        self.layer_2 = nn.Linear(400, 300)
+        self.layer_3 = nn.Linear(300, action_dim)
+        
+        self.max_action = max_action
+
+    def forward(self, x):
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = torch.tanh(self.layer_3(x)) * self.max_action
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        self.layer_1 = nn.Linear(state_dim + action_dim, 400)
+        self.layer_2 = nn.Linear(400, 300)
+        self.layer_3 = nn.Linear(300, 1)
+
+    def forward(self, x, u):
+        x = torch.cat([x, u], 1)
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = self.layer_3(x)
+        return x
+
+class ReplayBuffer:
+    def __init__(self, max_size=1e6):
+        self.storage = []
+        self.max_size = max_size
+        self.ptr = 0
+
+    def add(self, transition):
+        if len(self.storage) == self.max_size:
+            self.storage[int(self.ptr)] = transition
+            self.ptr = (self.ptr + 1) % self.max_size
+        else:
+            self.storage.append(transition)
+
+    def sample(self, batch_size):
+        ind = np.random.randint(0, len(self.storage), size=batch_size)
+        batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = [], [], [], [], []
+
+        for i in ind:
+            state, next_state, action, reward, done = self.storage[i]
+            batch_states.append(np.array(state, copy=False))
+            batch_next_states.append(np.array(next_state, copy=False))
+            batch_actions.append(np.array(action, copy=False))
+            batch_rewards.append(np.array(reward, copy=False))
+            batch_dones.append(np.array(done, copy=False))
+
+        return np.array(batch_states), np.array(batch_next_states), np.array(batch_actions), np.array(batch_rewards), np.array(batch_dones)
+
+class DDPG(object):
+    def __init__(self, state_dim, action_dim, max_action):
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = copy.deepcopy(self.actor)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = copy.deepcopy(self.critic)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+
+        self.max_action = max_action
+        self.replay_buffer = ReplayBuffer()
+
+    def select_action(self, state):
+        state = torch.Tensor(state.reshape(1, -1)).to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def train(self, batch_size=100, discount=0.99, tau=0.005):
+        state, next_state, action, reward, done = self.replay_buffer.sample(batch_size)
+        state = torch.Tensor(state).to(device)
+        next_state = torch.Tensor(next_state).to(device)
+        action = torch.Tensor(action).to(device)
+        reward = torch.Tensor(reward).to(device)
+        done = torch.Tensor(1 - done).to(device)
+
+        # Critic update
+        target_Q = self.critic_target(next_state, self.actor_target(next_state))
+        target_Q = reward + (done * discount * target_Q).detach()
+        current_Q = self.critic(state, action)
+        critic_loss = F.mse_loss(current_Q, target_Q)
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # Actor update
+        actor_loss = -self.critic(state, self.actor(state)).mean()
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # Update the frozen target models
+        for param, target_param in zip(self.critic.parameters(), self.c
